946b5bc794d4952eb10c62e66f1bd6a17a0d1000
Yolanda Diandari <yolandadiandari@gmail.com>
2021-03-31 15:38:33 +0700

    update tugas 5

--- I0320112_soal1_tugas5.py
+++ I0320112_soal1_tugas5.py
@@ -1,1 +1,11 @@
+#input nama dan jenis kelamin
+nama_pengunjung = input('Nama Pengunjung:')
+jenis_kelamin = input('Jenis kelamin:')
+
+#pengecekan data
+if jenis_kelamin == 'perempuan':
+    print('Selamat datang, Nyonya' + ' ' + nama_pengunjung)
+else:
+    print('Selamat datang, Tuan' + ' ' + nama_pengunjung)
+
 
--- I0320112_soal2_tugas5.py
+++ I0320112_soal2_tugas5.py
@@ -1,1 +1,20 @@
+#input nama dan nilai
+nama_siswa = input('Nama mahasiswa :')
+nilai = int(input('Masukkan nilai:'))
+
+#konversi nilai
+if nilai < 60:
+    print('Halo,' + ' ' + nama_siswa + '!' + ' ' + 'Nilai anda setelah dikonversi adalah' + ' ' + 'E')
+elif nilai <= 64:
+    print('Halo,' + ' ' + nama_siswa + '!' + ' ' + 'Nilai anda setelah dikonversi adalah' + ' ' + 'C')
+elif nilai <= 69:
+    print('Halo,' + ' ' + nama_siswa + '!' + ' ' + 'Nilai anda setelah dikonversi adalah' + ' ' + 'C+')
+elif nilai <= 74:
+    print('Halo,' + ' ' + nama_siswa + '!' + ' ' + 'Nilai anda setelah dikonversi adalah' + ' ' + 'B')
+elif nilai <= 79:
+    print('Halo,' + ' ' + nama_siswa + '!' + ' ' + 'Nilai anda setelah dikonversi adalah' + ' ' + 'B+')
+elif nilai <= 84:
+    print('Halo,' + ' ' + nama_siswa + '!' + ' ' + 'Nilai anda setelah dikonversi adalah' + ' ' + 'A-')
+else:
+    print('Halo,' + ' ' + nama_siswa + '!' + ' ' + 'Nilai anda setelah dikonversi adalah' + ' ' + 'A')
 
